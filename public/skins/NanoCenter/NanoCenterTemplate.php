4d860b5d60cd1dd27add4f0a38f136bfd1b53093
Jeff Kilroy <jeffkilroy@gmail.com>
2018-10-26 20:12:30 +0000

    update logo

--- public/skins/NanoCenter/NanoCenterTemplate.php
+++ public/skins/NanoCenter/NanoCenterTemplate.php
@@ -34,7 +34,8 @@ class NanoCenterTemplate extends BaseTemplate {
 		$html .= Html::rawElement( 'div', [ 'id' => 'mw-header-container', 'class' => 'ts-container' ],
 			Html::rawElement( 'div', [ 'id' => 'mw-header', 'class' => 'ts-inner' ],
 				''.//$userLinks['html'] .
-				$this->getLogo( 'p-logo-text', 'text' ) .
+                '<a id="p-banner" class="mw-wiki-title long" href="https://nanocenter.org">Nano Center Wiki</a>'.
+				//$this->getLogo( 'p-logo-text', 'text' ) .
 				''.$topNavigationHTML//$this->getSearch()
 			) .
 			$this->getClear()
